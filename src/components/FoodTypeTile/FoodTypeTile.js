d52308235d701c2a133ce017776aa69e32c6188c
thamash98 <thasaranga123@gmail.com>
2023-04-13 00:14:03 +0530

    Style for cursor in-out

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import Style from './FoodTypeTile.module.css'
+import React,{useState} from 'react';
+import Style from './FoodTypeTile.module.css';
 import breakfast_image from '../../assests/breakfast.png';
 
 const FoodTypeTile = () => {
+
+  const [mouseState, setMouseEntered]= useState(false);
+
+  const mouseEnterHandler = ()=>{
+      setMouseEntered(true);
+  }
+  const mouseLeaveHandler= ()=>{
+      setMouseEntered(false);
+  }
+
   return (
-    <div className={Style.outer}>
+    <div className={Style.outer} 
+         onMouseEnter={mouseEnterHandler}
+         onMouseLeave={mouseLeaveHandler}
+         style={{backgroundColor: !mouseState? "#B4FFF6":"#3a6d67"}}
+    >
         <img src={breakfast_image}></img>
-        <span className={Style.mainTitle}>Breakfast</span>
-        <span>13 Items</span>
+        <div className={Style.titleHolder}>
+          <span className={Style.mainTitle}>Breakfast</span>
+          <span className={Style.subTitle}>13 Items</span>
+        </div>
     </div>
   )
 }
